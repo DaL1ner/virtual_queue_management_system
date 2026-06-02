d6bb6c5a1d8a0ecac46df081aa21c58b1ae1907f
Daniil Romov <romovdany@mail.ru>
2026-06-02 17:11:48 +0300

    Попытка исправить ошибку в документации

--- mermaid_fence.py
+++
@@ -1,8 +0,0 @@
-# mermaid_fence.py
-def mermaid_format(source, language, css_class, options, md, attrs):
-    """Возвращает блок mermaid как есть, без обработки Pygments."""
-    # attrs может содержать дополнительные атрибуты, например, 'title'
-    title = attrs.get('title', '')
-    title_html = f'<div class="mermaid-title">{title}</div>' if title else ''
-    # Класс "mermaid" нужен для инициализации Mermaid.js
-    return f'{title_html}<pre class="mermaid">{source}</pre>'
